92bafe69028c7f1ca65b714179fea03e2fa82b54
Wito Chandra <wito.c.91@gmail.com>
2021-08-29 15:46:54 +0700

    facebook hacker cup 2020 - qualification round - problem a

---
+++ facebook/facebook-hacker-cup/2020/problem_a.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func canTravel(incomingRules, outgoingRules []bool, from, to int) bool {
+	distance := to - from
+	if distance == 0 {
+		return true
+	}
+	if distance == -1 || distance == 1 {
+		return incomingRules[to] && outgoingRules[from]
+	}
+	if distance < -1 {
+		return canTravel(incomingRules, outgoingRules, from, from-1) &&
+			canTravel(incomingRules, outgoingRules, from-1, to)
+	}
+	return canTravel(incomingRules, outgoingRules, from, from+1) &&
+		canTravel(incomingRules, outgoingRules, from+1, to)
+}
+
+func ProblemA(r io.Reader, w io.Writer) {
+	in := bufio.NewReader(r)
+	out := bufio.NewWriter(w)
+	defer out.Flush()
+
+	numberOfAirlines := 0
+	fmt.Fscanln(in, &numberOfAirlines)
+	for caseNumber := 1; caseNumber <= numberOfAirlines; caseNumber++ {
+		numberOfCountries := 0
+		incomingRulesStr := ""
+		outgoingRulesStr := ""
+
+		fmt.Fscanln(in, &numberOfCountries)
+		fmt.Fscanln(in, &incomingRulesStr)
+		fmt.Fscanln(in, &outgoingRulesStr)
+		incomingRules := make([]bool, numberOfCountries)
+		outgoingRules := make([]bool, numberOfCountries)
+
+		for i := 0; i < numberOfCountries; i++ {
+			incomingRules[i] = incomingRulesStr[i] == 'Y'
+			outgoingRules[i] = outgoingRulesStr[i] == 'Y'
+		}
+
+		results := make([]string, 0)
+		for i := 0; i < numberOfCountries; i++ {
+			result := make([]string, 0)
+			for j := 0; j < numberOfCountries; j++ {
+				if canTravel(incomingRules, outgoingRules, i, j) {
+					result = append(result, "Y")
+				} else {
+					result = append(result, "N")
+				}
+			}
+			results = append(results, strings.Join(result, ""))
+		}
+
+		fmt.Fprintf(out, "Case #%d:\n", caseNumber)
+		fmt.Fprintf(out, "%s\n", strings.Join(results, "\n"))
+	}
+}
---
+++ facebook/facebook-hacker-cup/2020/problem_a_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProblemA_Small(t *testing.T) {
+	input := `
+5
+2
+YY
+YY
+2
+NY
+YY
+2
+NN
+YY
+5
+YNNYY
+YYYNY
+10
+NYYYNNYYYY
+YYNYYNYYNY
+`
+
+	expected := `
+Case #1:
+YY
+YY
+Case #2:
+YY
+NY
+Case #3:
+YN
+NY
+Case #4:
+YNNNN
+YYNNN
+NNYYN
+NNNYN
+NNNYY
+Case #5:
+YYYNNNNNNN
+NYYNNNNNNN
+NNYNNNNNNN
+NNYYNNNNNN
+NNYYYNNNNN
+NNNNNYNNNN
+NNNNNNYYYN
+NNNNNNYYYN
+NNNNNNNNYN
+NNNNNNNNYY
+`
+
+	inputReader := strings.NewReader(strings.TrimSpace(input))
+	output := &strings.Builder{}
+
+	ProblemA(inputReader, output)
+	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(output.String()))
+}
