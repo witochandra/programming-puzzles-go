71344e80a449178126f999e6b42ccf79478015eb
Wito Chandra <wito.c.91@gmail.com>
2021-08-30 14:26:13 +0000

    solve problem a 2

---
+++ facebook/facebook-hacker-cup/2021/qualification/problem_a_2.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Node struct {
+	Name     rune
+	Children []*Node
+}
+
+func (n *Node) AddChild(child *Node) {
+	for _, c := range n.Children {
+		if c.Name == child.Name {
+			return
+		}
+	}
+	n.Children = append(n.Children, child)
+}
+
+func NewNode(name rune) *Node {
+	return &Node{
+		Name:     name,
+		Children: make([]*Node, 0),
+	}
+}
+
+func DistanceTo(toName rune, skips string, nodesToCheck []*Node) int {
+	nextNodeToCheckSet := make(map[rune]bool)
+	nextNodesToCheck := make([]*Node, 0)
+	for _, node := range nodesToCheck {
+		if node.Name == toName {
+			return 1
+		}
+		for _, gc := range node.Children {
+			if strings.ContainsRune(skips, gc.Name) {
+				continue
+			}
+			skips = skips + string(gc.Name)
+			if !nextNodeToCheckSet[gc.Name] {
+				nextNodeToCheckSet[gc.Name] = true
+				nextNodesToCheck = append(nextNodesToCheck, gc)
+			}
+		}
+	}
+	if len(nextNodeToCheckSet) > 0 {
+		distance := DistanceTo(toName, skips, nextNodesToCheck)
+		if distance >= 0 {
+			return 1 + distance
+		}
+	}
+
+	return -1
+}
+
+func ProblemA2(r io.Reader, w io.Writer) {
+	in := bufio.NewReader(r)
+	out := bufio.NewWriter(w)
+	defer out.Flush()
+
+	numberOfBirthdays := 0
+	fmt.Fscanln(in, &numberOfBirthdays)
+
+	for caseNo := 1; caseNo <= numberOfBirthdays; caseNo++ {
+		input := ""
+		replacementCount := 0
+		fmt.Fscanln(in, &input)
+		fmt.Fscanln(in, &replacementCount)
+
+		nodes := make(map[rune]*Node)
+		runeCount := make(map[rune]int)
+		for _, v := range input {
+			nodes[v] = NewNode(v)
+			runeCount[v] = runeCount[v] + 1
+		}
+		sameRuneCount := 1
+		for _, count := range runeCount {
+			if sameRuneCount < count {
+				sameRuneCount = count
+			}
+		}
+		for i := 0; i < replacementCount; i++ {
+			tmp := ""
+			fmt.Fscanln(in, &tmp)
+			runes := []rune(tmp)
+			destination, found := nodes[runes[1]]
+			if !found {
+				destination = NewNode(runes[1])
+				nodes[runes[1]] = destination
+			}
+			source, found := nodes[runes[0]]
+			if !found {
+				source = NewNode(runes[0])
+				nodes[runes[0]] = source
+			}
+			source.AddChild(destination)
+		}
+		currentSteps := -1
+		minSteps := len(input) - sameRuneCount
+		for destination := range nodes {
+			steps := 0
+			finish := true
+			for _, source := range input {
+				if source == destination {
+					continue
+				}
+				distance := DistanceTo(destination, string(source), nodes[source].Children)
+				steps = steps + distance
+				if distance < 0 {
+					finish = false
+					break
+				}
+			}
+			if finish && (currentSteps == -1 || steps < currentSteps) {
+				currentSteps = steps
+				if currentSteps == minSteps {
+					break
+				}
+			}
+		}
+		fmt.Fprintf(out, "Case #%d: %d\n", caseNo, currentSteps)
+	}
+}
+
+// func main() {
+// 	ProblemA2(os.Stdin, os.Stdout)
+// }
---
+++ facebook/facebook-hacker-cup/2021/qualification/problem_a_2_test.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProblemA2(t *testing.T) {
+	input := func() string {
+		return `
+7
+ABC
+2
+BA
+CA
+ABC
+2
+AB
+AC
+F
+0
+BANANA
+4
+AB
+AN
+BA
+NA
+FBHC
+4
+FB
+BF
+HC
+CH
+FOXEN
+8
+NI
+OE
+NX
+EW
+OI
+FE
+FN
+XW
+CONSISTENCY
+26
+AB
+BC
+CD
+DE
+EF
+FG
+GH
+HI
+IJ
+JK
+KL
+LM
+MN
+NO
+OP
+PQ
+QR
+RS
+ST
+TU
+UV
+VW
+WX
+XY
+YZ
+ZA
+		`
+	}()
+	expected := func() string {
+		return `
+Case #1: 2
+Case #2: -1
+Case #3: 0
+Case #4: 3
+Case #5: -1
+Case #6: 8
+Case #7: 100
+`
+	}()
+
+	reader := strings.NewReader(strings.TrimSpace(input))
+	writer := &strings.Builder{}
+
+	ProblemA2(reader, writer)
+
+	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(writer.String()))
+}
