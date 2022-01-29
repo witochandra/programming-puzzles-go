54656b42e56848b7f24d480ffa26993f4b926f95
Wito Chandra <wito.c.91@gmail.com>
2022-01-29 10:29:50 +0000

    solve aoc day 4

---
+++ adventofcode/2021/day04.go
@@ -0,0 +1,149 @@
+package adventofcode
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+func Problem2021Day04(r io.Reader, w io.Writer, part int) {
+	type Board struct {
+		Numbers      []int
+		MarksByIndex map[int]bool
+	}
+
+	createBoard := func(numbers []int) *Board {
+		board := &Board{
+			Numbers:      numbers,
+			MarksByIndex: make(map[int]bool),
+		}
+		return board
+	}
+
+	mark := func(board *Board, number int) bool {
+		for i, n := range board.Numbers {
+			if n == number {
+				board.MarksByIndex[i] = true
+				return true
+			}
+		}
+		return false
+	}
+
+	isBoardWinning := func(board *Board) bool {
+		if len(board.MarksByIndex) < 5 {
+			return false
+		}
+		// check whether there is a completed row
+		for _, i := range []int{0, 5, 10, 15, 20} {
+			count := 0
+			for j := 0; j < 5; j++ {
+				if !board.MarksByIndex[i+j] {
+					break
+				}
+				count++
+			}
+			if count == 5 {
+				return true
+			}
+		}
+		// check whether there is a completed column
+		for i := 0; i < 5; i++ {
+			count := 0
+			for j := 0; j < 5; j++ {
+				if !board.MarksByIndex[(j*5)+i] {
+					break
+				}
+				count++
+			}
+			if count == 5 {
+				return true
+			}
+		}
+		return false
+	}
+
+	calculateScore := func(board *Board, multiplier int) int {
+		sum := 0
+		for i, n := range board.Numbers {
+			if board.MarksByIndex[i] {
+				continue
+			}
+			sum += n
+		}
+		return sum * multiplier
+	}
+
+	in := bufio.NewReader(r)
+	out := bufio.NewWriter(w)
+	defer out.Flush()
+
+	incomingNumbers := func() []int {
+		numberString := ""
+		fmt.Fscanln(in, &numberString)
+		numbers := make([]int, 0)
+		for _, v := range strings.Split(numberString, ",") {
+			n, _ := strconv.Atoi(v)
+			numbers = append(numbers, n)
+		}
+		return numbers
+	}()
+
+	boards := make([]*Board, 0)
+	for {
+		_, err := in.Peek(1)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Fscanln(in)
+		numbersInBoard := make([]int, 0)
+		for i := 0; i < 5; i++ {
+			numbers := make([]int, 5)
+			fmt.Fscanln(in, &numbers[0], &numbers[1], &numbers[2], &numbers[3], &numbers[4])
+			numbersInBoard = append(numbersInBoard, numbers...)
+		}
+		boards = append(boards, createBoard(numbersInBoard))
+	}
+
+	score := 0
+	findFirstWinningBoardScore := func() int {
+		for _, currentNumber := range incomingNumbers {
+			for _, board := range boards {
+				mark(board, currentNumber)
+				if isBoardWinning(board) {
+					return calculateScore(board, currentNumber)
+				}
+			}
+		}
+		return -1
+	}
+	findLastWinningBoardScore := func() int {
+		winningBoardByIndex := make(map[int]bool)
+		for _, currentNumber := range incomingNumbers {
+			for i, board := range boards {
+				if winningBoardByIndex[i] {
+					continue
+				}
+				mark(board, currentNumber)
+				if isBoardWinning(board) {
+					winningBoardByIndex[i] = true
+					if len(winningBoardByIndex) == len(boards) {
+						return calculateScore(board, currentNumber)
+					}
+				}
+			}
+		}
+		return -1
+	}
+	if part == 1 {
+		score = findFirstWinningBoardScore()
+	} else if part == 2 {
+		score = findLastWinningBoardScore()
+	} else {
+		panic("invalid part")
+	}
+	fmt.Fprintln(out, score)
+}
---
+++ adventofcode/2021/day04_test.go
@@ -0,0 +1,82 @@
+package adventofcode
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProblem2021Day04_Example(t *testing.T) {
+	input := strings.TrimSpace(`
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+`)
+	t.Run("part 1", func(t *testing.T) {
+		expected := "4512"
+
+		reader := strings.NewReader(input)
+		writer := &strings.Builder{}
+
+		Problem2021Day04(reader, writer, 1)
+
+		assert.Equal(t, expected, strings.TrimSpace(writer.String()))
+	})
+	t.Run("part 2", func(t *testing.T) {
+		expected := "1924"
+
+		reader := strings.NewReader(input)
+		writer := &strings.Builder{}
+
+		Problem2021Day04(reader, writer, 2)
+
+		assert.Equal(t, expected, strings.TrimSpace(writer.String()))
+	})
+}
+
+func TestProblem2021Day04_Puzzle(t *testing.T) {
+	fileContent, err := os.ReadFile("./day04_input.txt")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	input := strings.TrimSpace(string(fileContent))
+	t.Run("part 1", func(t *testing.T) {
+		expected := "41503"
+
+		reader := strings.NewReader(input)
+		writer := &strings.Builder{}
+
+		Problem2021Day04(reader, writer, 1)
+
+		assert.Equal(t, expected, strings.TrimSpace(writer.String()))
+	})
+	t.Run("part 2", func(t *testing.T) {
+		expected := "3178"
+
+		reader := strings.NewReader(input)
+		writer := &strings.Builder{}
+
+		Problem2021Day04(reader, writer, 2)
+
+		assert.Equal(t, expected, strings.TrimSpace(writer.String()))
+	})
+}
